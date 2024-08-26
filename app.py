46e2c5d1ed4e85cd74660daa5efd72c390968b4d
LijuanHao <haol0005@e.ntu.edu.sg>
2024-08-26 04:00:43 +0000

    @app.route("/creditability",methods=["GET","POST"]) def creditability():     return(render_template("creditability.html"))
    @app.route("/creditability_prediction",methods=["GET","POST"])
    def creditabiity_prediction():
        q = float(request.form.get("q"))
        return(render_template("credictability_prediction.html",r=1.22937616 + (-0.00011189*q)))

--- app.py
+++ app.py
@@ -42,6 +42,15 @@ def DBS_prediction():
     q = float(request.form.get("q"))
     return(render_template("DBS_prediction.html",r=90.2 + (-50.6*q)))
 
+@app.route("/creditability",methods=["GET","POST"])
+def creditability():
+    return(render_template("creditability.html"))
+
+@app.route("/creditability_prediction",methods=["GET","POST"])
+def creditabiity_prediction():
+    q = float(request.form.get("q"))
+    return(render_template("credictability_prediction.html",r=1.22937616 + (-0.00011189*q)))
+
 @app.route("/makersuite",methods=["GET","POST"])
 def makersuite():
     return(render_template("makersuite.html"))
